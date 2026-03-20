828aaf4613f06da10c4057eeee846e59e44131d4
Harley Jones <harleybrendanjones@gmail.com>
2026-03-20 02:15:56 +0000

    Fix run list view and template

--- runs/views.py
+++ runs/views.py
@@ -3,6 +3,6 @@ from django.views import generic
 from .models import Run
 
 # Create your views here.
-class PostList(generic.ListView):
+class RunList(generic.ListView):
     queryset = Run.objects.all()
-    template_name = "post_list.html"
+    template_name = "run_list.html"
