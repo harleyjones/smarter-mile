9c45540691c1539585ff7aeb910a929c50baf44b
Harley Jones <harleybrendanjones@gmail.com>
2026-03-20 00:44:47 +0000

    Add comment model and register it in admin.py

--- runs/admin.py
+++ runs/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Run
+from .models import Run, Comment
 
 # Register your models here.
 admin.site.register(Run)
+admin.site.register(Comment)
--- runs/models.py
+++ runs/models.py
@@ -6,8 +6,19 @@ class Run(models.Model):
     title = models.CharField(max_length=200, blank=False)
     slug = models.SlugField(max_length=200, unique=True)
     author = models.ForeignKey(
-    User, on_delete=models.CASCADE, related_name="run_posts")
+        User, on_delete=models.CASCADE, related_name="run_posts")
     created_on = models.DateTimeField(auto_now_add=True)
     distance = models.FloatField()
     duration = models.DurationField()
+    updated_on = models.DateTimeField(auto_now=True)
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(
+        Run, on_delete=models.CASCADE, related_name="comments")
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="commenter")
+    body = models.TextField()
+    approved = models.BooleanField(default=True)
+    created_on = models.DateTimeField(auto_now_add=True)
     updated_on = models.DateTimeField(auto_now=True)
