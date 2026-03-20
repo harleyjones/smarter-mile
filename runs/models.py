e97bac38347f9e163abcf1ec710f15ef8c0ab32d
Harley Jones <harleybrendanjones@gmail.com>
2026-03-20 00:31:59 +0000

    Add Run model and admin registration

--- runs/models.py
+++ runs/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Run(models.Model):
+    title = models.CharField(max_length=200, blank=False)
+    slug = models.SlugField(max_length=200, unique=True)
+    author = models.ForeignKey(
+    User, on_delete=models.CASCADE, related_name="run_posts")
+    created_on = models.DateTimeField(auto_now_add=True)
+    distance = models.FloatField()
+    duration = models.DurationField()
--- smartmile/settings.py
+++ smartmile/settings.py
@@ -24,10 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-(gzpa^k1dgdldr)rb8!pi-@o)irur%ovig9j9ux_lab&g4293q'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.herokuapp.com']
 
@@ -82,6 +82,10 @@ DATABASES = {
     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
 
+CSRF_TRUSTED_ORIGINS = [
+    "https://*.codeinstitute-ide.net/",
+    "https://*.herokuapp.com"
+]
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
