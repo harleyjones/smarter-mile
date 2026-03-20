b458d5c9b2c94c7ac8946a041bece61425a377f0
Harley Jones <harleybrendanjones@gmail.com>
2026-03-20 09:26:23 +0000

    added run_detail view

--- runs/urls.py
+++ runs/urls.py
@@ -3,4 +3,5 @@ from django.urls import path
 
 urlpatterns = [
     path("", views.RunList.as_view(), name='home'),
+    path('<slug:slug>/', views.run_detail, name='run_detail')
 ]
--- runs/views.py
+++ runs/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from .models import Run
 
@@ -7,3 +7,26 @@ class RunList(generic.ListView):
     queryset = Run.objects.all()
     template_name = "runs/index.html"
     paginate_by = 6
+
+def run_detail(request, slug):
+    """
+    Display an individual :model:`runs.Run`.
+
+    **Context**
+
+    ``run``
+        An instance of :model:`runs.Run`.
+
+    **Template:**
+
+    :template:`runs/run_detail.html`
+    """
+
+    queryset = Run.objects.filter(status=1)
+    run = get_object_or_404(queryset, slug=slug)
+
+    return render(
+        request,
+        "runs/run_detail.html",
+        {"run": run},
+    )
